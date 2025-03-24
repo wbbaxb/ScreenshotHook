a2edea691bc29503e2d513d3dbb933529406a72f
6324 <724301996@qq.com>
2025-03-24 20:38:20 +0800

    添加基础框架

---
+++ HookLibrary/MainHook.cs
@@ -0,0 +1,71 @@
+﻿using EasyHook;
+using Framework;
+using System.Drawing;
+
+namespace HookLibrary
+{
+    public class MainHook : IEntryPoint
+    {
+        private string _watermark;
+
+        public MainHook(RemoteHooking.IContext context, string watermark)
+        {
+            _watermark = watermark;
+        }
+
+        public void Run(RemoteHooking.IContext context, string watermark)
+        {
+            var hook = LocalHook.Create(LocalHook.GetProcAddress("gdi32.dll", "BitBlt"),
+                new Win32.BitBltDelegate(BitBlt_Hooked),
+                this);
+
+            hook.ThreadACL.SetExclusiveACL(new int[] { 0 });
+            RemoteHooking.WakeUpProcess();
+            Thread.Sleep(-1);
+        }
+
+        // 劫持后的 BitBlt
+        private bool BitBlt_Hooked(IntPtr hdcDest, int xDest, int yDest, int w, int h, IntPtr hdcSrc, int xSrc, int ySrc, int rop)
+        {
+            bool ret = Win32.BitBlt(hdcDest, xDest, yDest, w, h, hdcSrc, xSrc, ySrc, rop);
+
+            var text = _watermark + " " + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+
+            using (var graphics = Graphics.FromHdc(hdcDest))
+            {
+                // 1) 测量文本大小，用于确定水印间隔
+                SizeF textSize = graphics.MeasureString(text, new Font("宋体", 20, FontStyle.Regular));
+                float textWidth = textSize.Width;
+                float textHeight = textSize.Height;
+
+                // 2) 平移 + 旋转，使后续文字呈对角斜
+                //    先把坐标原点移到屏幕中心，再旋转 -46.5 度
+                graphics.TranslateTransform(w / 2, h / 2);
+                graphics.RotateTransform(-46.5f);
+
+                // 3) 在大的坐标范围内循环绘制同样的文字
+                //    让水印覆盖到整个图片区域
+                //    “-bitmap.Width” 到 “bitmap.Width” 是为了保证
+                //    在旋转坐标系中，四角也都有水印
+                int stepX = (int)(textWidth + 130);   // 两列水印之间的水平间隔
+                int stepY = (int)(textHeight + 160);  // 两行水印之间的垂直间隔
+
+                for (int y = -h; y < h; y += stepY)
+                {
+                    for (int x = -w; x < w; x += stepX)
+                    {
+                        graphics.DrawString(text,
+                            new Font("宋体", 20, FontStyle.Regular),
+                            new SolidBrush(Color.FromArgb(130, Color.Gray)),
+                            x, y);
+                    }
+                }
+
+                // 4) 重置坐标变换，避免影响后面其它绘制
+                graphics.ResetTransform();
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ ScreenshotHook/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using EasyHook;
+using Framework;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace ScreenshotHook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Hook();
+        }
+
+        private void Hook()
+        {
+            var dllDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dlls");
+
+            if (!Directory.Exists(dllDir))
+            {
+                MessageBox.Show("Dll目录不存在");
+                return;
+            }
+
+            string dllName = Environment.Is64BitProcess ? "EasyHook64.dll" : "EasyHook32.dll";
+            IntPtr hModule = Win32.LoadLibrary(Path.Combine(dllDir, dllName));
+
+            if (hModule == IntPtr.Zero)
+            {
+                // LoadLibrary 失败，检查错误码
+                int error = Marshal.GetLastWin32Error();
+
+                if (error != 0)
+                {
+                    MessageBox.Show("Dll路径设置失败：" + error);
+                    return;
+                }
+            }
+
+            string dllPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "HookLibrary.dll");
+
+            if (!File.Exists(dllPath))
+            {
+                MessageBox.Show("dll路径错误！");
+                return;
+            }
+
+            try
+            {
+                string processName = "wechat";
+                var processes = Process.GetProcessesByName(processName);
+
+                if (processes.Length == 0)
+                {
+                    MessageBox.Show($"{processName} 未运行！");
+                    return;
+                }
+
+                var process = processes[0];
+
+                var currentPlat = Environment.Is64BitProcess ? 64 : 32;
+                var targetPlat = Utilities.Is64BitProcess(process) ? 64 : 32;
+
+                if (currentPlat != targetPlat)
+                {
+                    MessageBox.Show(string.Format("当前程序是{0}位程序，目标进程是{1}位程序，" +
+                        "请调整编译选项重新编译后重试！", currentPlat, targetPlat));
+                    return;
+                }
+
+                try
+                {
+                    RemoteHooking.Inject(
+                        process.Id,
+                        InjectionOptions.Default,
+                        dllPath,
+                        dllPath,
+                        "test"
+                    );
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"进程ID:{process.Id}:Hook失败:{ex.ToString()}");
+                    return;
+                }
+
+                MessageBox.Show("Hook 成功！");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("注入失败: " + ex.Message);
+            }
+        }
+    }
+}
