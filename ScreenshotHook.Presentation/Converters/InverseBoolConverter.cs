9c01766306ac81e357d59622b0efde9084828f00
6324 <724301996@qq.com>
2025-05-07 21:10:57 +0800

    feat: implement prevent repeat injection

---
+++ ScreenshotHook.Presentation/Converters/InverseBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ScreenshotHook.Presentation.Converters
+{
+    internal class InverseBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool val)
+            {
+                return !val;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
