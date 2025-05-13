3169ac29638442887ddbae5ae89c31076b04abfc
6324 <724301996@qq.com>
2025-05-13 10:52:20 +0800

    feat: integrate log4net and add global exception handling

--- ScreenshotHook.Presentation/App.xaml.cs
+++ ScreenshotHook.Presentation/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using ScreenshotHook.Presentation.Utilities;
+using System;
 using System.Reflection;
 using System.Threading;
 using System.Windows;
@@ -14,19 +15,35 @@ namespace ScreenshotHook.Presentation
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            LogHelper.Configure();
             base.OnStartup(e);
 
             // 创建互斥体
             string mutexName = Assembly.GetExecutingAssembly().GetName().Name;
-            bool createdNew;
-            mutex = new Mutex(true, mutexName, out createdNew);
+            mutex = new Mutex(true, mutexName, out bool createdNew);
 
             if (createdNew)
             {
-                // TODO:
+                //非UI线程中未处理的异常
+                AppDomain.CurrentDomain.UnhandledException += (s, args) =>
+                {
+                    if (args.ExceptionObject is Exception ex)
+                    {
+                        LogHelper.Error(ex);
+                        LogHelper.Info("程序即将异常关闭！");
+                    }
+                };
+
+                //UI线程异常
+                this.DispatcherUnhandledException += (s, args) =>
+                {
+                    LogHelper.Error(args.Exception);
+                    args.Handled = true;
+                };
             }
             else
             {
+                LogHelper.Info("检测到实例已运行，退出当前实例。");
                 //给已启动的程序发送自定义消息（显示并置顶窗口）
                 var windowHandle = Win32.FindWindow(null, ViewModels.MainWindowViewModel.Title);
                 Win32.SendMessage(windowHandle, Views.MainWindow.WM_RESTOREWINDOW, IntPtr.Zero, IntPtr.Zero);
---
+++ ScreenshotHook.Presentation/Utilities/LogHelper.cs
@@ -0,0 +1,57 @@
+﻿using log4net;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace ScreenshotHook.Presentation.Utilities
+{
+    public static class LogHelper
+    {
+        // 使用当前类型获取 Logger
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void Configure()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+
+            var configFile = new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log4net.config"));
+
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(logRepository, configFile);
+        }
+
+        public static void Info(object message)
+        {
+            if (log.IsInfoEnabled)
+                log.Info(message);
+        }
+
+        public static void Warn(object message)
+        {
+            if (log.IsWarnEnabled)
+                log.Warn(message);
+        }
+
+        public static void Error(object message, Exception ex = null)
+        {
+            if (log.IsErrorEnabled)
+            {
+                if (ex != null)
+                {
+                    log.Error(message, ex);
+                }
+                else
+                {
+                    log.Error(message);
+                }
+            }
+        }
+
+        public static void Error(Exception ex)
+        {
+            if (log.IsErrorEnabled)
+            {
+                log.Error(ex.Message, ex);
+            }
+        }
+    }
+}
